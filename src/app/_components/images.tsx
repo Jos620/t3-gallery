b0872e5c03fa8d18ebd5d2d9306448f09007d5bd
Mateus Ito <mateusito10@gmail.com>
2024-07-21 18:26:06 -0300

    perf: setup next/image for images

--- src/app/_components/images.tsx
+++ src/app/_components/images.tsx
@@ -1,16 +1,17 @@
 import { getMyImages } from "@/server/queries";
+import Image from "next/image";
 
 export async function Images() {
   const images = await getMyImages();
 
   return (
-    <div className="flex flex-wrap gap-4">
+    <div className="flex flex-wrap justify-center gap-4">
       {images.map((img) => (
         <div
           key={img.id}
-          className="flex h-max w-48 flex-col gap-1 bg-foreground p-1 text-background"
+          className="flex h-max flex-col gap-1 bg-foreground p-1 text-background"
         >
-          <img src={img.url} alt="" />
+          <Image src={img.url} alt={img.name} width={190} height={190} />
           <p>{img.name}</p>
         </div>
       ))}
