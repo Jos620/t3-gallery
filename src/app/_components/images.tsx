915de392b7c51e31011bb68ab43db017a4a84fbb
Mateus Ito <mateusito10@gmail.com>
2024-07-21 15:41:10 -0300

    chore(auth): hide images from unauthenticated users

---
+++ src/app/_components/images.tsx
@@ -0,0 +1,21 @@
+import { db } from "@/server/db";
+
+export async function Images() {
+  const images = await db.query.image.findMany({
+    orderBy: (model, { desc }) => desc(model.id),
+  });
+
+  return (
+    <div className="flex flex-wrap gap-4">
+      {images.map((img) => (
+        <div
+          key={img.id}
+          className="flex h-max w-48 flex-col gap-1 bg-foreground p-1 text-background"
+        >
+          <img src={img.url} alt="" />
+          <p>{img.name}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,18 @@
-import { db } from "@/server/db";
+import { SignedIn, SignedOut } from "@clerk/nextjs";
+import { Images } from "./_components/images";
 
 export const dynamic = "force-dynamic";
 
 export default async function HomePage() {
-  const images = await db.query.image.findMany({
-    orderBy: (model, { desc }) => desc(model.id),
-  });
-
   return (
     <div className="flex flex-col gap-4">
-      <div className="flex flex-wrap gap-4">
-        {images.map((img) => (
-          <div
-            key={img.id}
-            className="bg-foreground text-background flex h-max w-48 flex-col gap-1 p-1"
-          >
-            <img src={img.url} alt="" />
-            <p>{img.name}</p>
-          </div>
-        ))}
-      </div>
+      <SignedOut>
+        <p className="text-center text-2xl">Please sign in above</p>
+      </SignedOut>
+
+      <SignedIn>
+        <Images />
+      </SignedIn>
     </div>
   );
 }
